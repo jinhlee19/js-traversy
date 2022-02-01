81860af1aaec427a0d49ac8cde28542a8d48effb
rafy <jinhlee19@gmail.com>
2022-02-02 00:51:14 +0900

    String Method & Concatenation

---
+++ 1-intro/js/strmethod.js
@@ -0,0 +1,58 @@
+const fName = "John";
+const lName = "Snow";
+const age = 25;
+let val;
+const str = "hello there my name is Rafy";
+const tags = "web design,web develeopment,programming";
+
+//// Concatenation
+val = fName + " " + lName;
+
+//// Append
+val = "Arya ";
+val += "Stark";
+
+val = "His name is " + fName + " and he is " + age;
+
+//// Escaping
+val = "That's awesome, I can't wait";
+val = "That's awesome, I can't wait";
+
+//// length - property
+val = fName.length;
+
+//// concat() - method
+val = fName.concat(" ", lName);
+
+//// change case
+val = fName.toUpperCase();
+val = fName.toLowerCase();
+
+//// index, indexOf()
+herName = "Arya Stark";
+val = herName[0];
+val = herName.indexOf("h");
+val = herName.lastIndexOf("a");
+// 0 으로 시작하므로 length -1 로 마지막 숫자를 찾는다. common formula you will see in JS
+val = fName.charAt(fName.length - 1); // n (from 'John')
+
+//// substring()
+val = fName.substring(1, 3);
+
+//// slice()
+val = fName.slice(1, 3);
+
+// start from back to last three
+val = fName.slice(-3);
+
+// split()
+val = str.split(" ");
+val = tags.split(",");
+
+//// replace()
+val = str.replace('Rafy', 'Jack');
+
+/// includes()
+val =  str.includes('my');
+
+console.log(val);
--- 1-intro/js/typeconversion.js
+++ 1-intro/js/typeconversion.js
@@ -5,7 +5,7 @@ let val;
 
 // Number to string
 val = String(555); // '555', string, 3
-val = String(4+4); // '8', string, 1
+val = String(4 + 4); // '8', string, 1
 
 // Bool to String
 val = String(true); // 'true', string, 4
@@ -14,37 +14,34 @@ val = String(true); // 'true', string, 4
 val = String(new Date()); // 'Tue Feb 01 2022 04:54:53 GMT+0900 (한국 표준시)'
 
 // Array to String
-val = String([1,2,3,4]); // 1,2,3,4, string, 7
+val = String([1, 2, 3, 4]); // 1,2,3,4, string, 7
 
 // toString()
 val = (5).toString(); // 5, string, 1
-val = (true).toString(); // true, string, 4
-
+val = true.toString(); // true, string, 4
 
 // String to number
-val = Number('5');
+val = Number("5");
 // decimal point == 2
 console.log(val.toFixed(2)); // 5.00
 
 val = Number(true); // 1, number, 1
 val = Number(false); // 0, number, 0
 val = Number(null); // 0, number, 0
-val = Number('hello'); // NAN, number, NAN: actual value that returns not a number
+val = Number("hello"); // NAN, number, NAN: actual value that returns not a number
 
-val = parseInt('100.30'); // 100 (parse only intenger)
-val = parseFloat('100.31'); // 100.31(parse with decimals)
+val = parseInt("100.30"); // 100 (parse only intenger)
+val = parseFloat("100.31"); // 100.31(parse with decimals)
 
 // Output
-console.log(val); 
-console.log(typeof val); 
-console.log(val.length); 
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 console.log(val.toFixed(2));
 
 //// Type Coercion - What js do 'String' + 'int'
 const val1 = 4564;
-const val2 = ' text';
-const sum= val1 + val2;
+const val2 = " text";
+const sum = val1 + val2;
 console.log(sum);
 console.log(typeof sum);
-
-

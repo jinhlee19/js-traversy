c25a769c7b6ee5fbefc163909749728269bb7f1c
rafy <jinhlee19@gmail.com>
2022-02-05 23:23:47 +0900

    Task List - UI

---
+++ 3-Dom-Projects/starter copy/app.js
@@ -0,0 +1,16 @@
+// Define UI Vars
+const from = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+
+loadEventListeners();
+
+// Load all event listerners
+function loadEventListeners() {
+    //Add task event
+    from.addEventListener('submit');
+}

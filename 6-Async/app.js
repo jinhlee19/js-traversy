fc5996086f732349753971fa3f7de486e23c80f4
rafy <jinhlee19@gmail.com>
2022-02-10 15:10:42 +0900

    Asynchronous Javascript, Ajax & Fetch API

---
+++ 6-Async/app.js
@@ -0,0 +1,22 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData(){
+    // Create an XHR Object
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', 'data.txt', true);
+    xhr.onload = function(){
+        if(this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+    xhr.send();
+    // 아무튼 ajax랑 XHR이 더 복잡함. fetch가 newer std.
+
+    // OPEN - specify type of request and url
+    
+    // HTTP STATUS
+    // 200: "OK"
+    // 403: "Forbidden"
+    // 404: "Not Found"
+}

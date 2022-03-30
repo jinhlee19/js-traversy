b6345b720830ce6a84c269e222f1f8bd7e77a469
rafy <jinhlee19@gmail.com>
2022-03-30 22:17:23 +0900

    Show alert fix

--- 7-API-Projects/1-github-finder/ui.js
+++ 7-API-Projects/1-github-finder/ui.js
@@ -31,3 +31,27 @@ class UI {
       `;
     }
   }
+
+  // Show alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div  =  document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container =  document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+  
